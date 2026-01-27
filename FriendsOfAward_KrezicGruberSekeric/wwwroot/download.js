4cded8c52029a7f75a54814e5dbb3c43f4af272b
Marcoste69 <marcogruber399@gmail.com>
2026-01-27 12:37:46 +0100

    finished

---
+++ FriendsOfAward_KrezicGruberSekeric/wwwroot/download.js
@@ -0,0 +1,11 @@
+﻿window.downloadHtmlFile = (filename, content) => {
+    const blob = new Blob([content], { type: "text/html;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+
+    URL.revokeObjectURL(url);
+};
